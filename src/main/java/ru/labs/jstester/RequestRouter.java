26a9c813dfb8ee946ec6f9d8acf1409782354c9e
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-02 02:52:38 +0300

    Scripted auto-commit on change (2019-11-02 02:52:38) by gitwatch.sh

--- src/main/java/ru/labs/jstester/RequestRouter.java
+++ src/main/java/ru/labs/jstester/RequestRouter.java
@@ -26,7 +26,7 @@ public class RequestRouter extends AbstractActor {
 
                     for (Test test : r.getTests()) {
                         testRunnersPool.tell(
-                                new TestTask(r.getMeta(), test), getSelf());
+                                new TestTask(new TestMetaInfo(r.getPackageID(), r.getJsCode(), r.getFunctionName()), test), getSelf());
                     }
                 })
                 .match(ResultRequest.class, r -> {
