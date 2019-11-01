2a3779803563d0320e47ea9845d972e9127abb43
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-01 23:31:31 +0300

    Scripted auto-commit on change (2019-11-01 23:31:31) by gitwatch.sh

--- src/main/java/ru/labs/jstester/ResultsStorage.java
+++ src/main/java/ru/labs/jstester/ResultsStorage.java
@@ -12,7 +12,7 @@ public class ResultsStorage extends AbstractActor {
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(TestResult.class, r -> {
-                    innerStorage.put(m.getKey(), m.getValue());
+                    innerStorage.put(, m.getValue());
                     System.out.println(String.format("message: K: %s, V: %s", m.getKey(), m.getValue()));
                 })
                 .build();
