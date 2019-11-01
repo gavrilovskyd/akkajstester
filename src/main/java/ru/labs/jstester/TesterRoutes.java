e8b972552a10ba6a380e4e81716967059bfa41fc
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-02 00:37:53 +0300

    Scripted auto-commit on change (2019-11-02 00:37:53) by gitwatch.sh

--- src/main/java/ru/labs/jstester/TesterRoutes.java
+++ src/main/java/ru/labs/jstester/TesterRoutes.java
@@ -4,7 +4,7 @@ import akka.actor.ActorRef;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.PathMatchers;
 import akka.http.javadsl.server.Route;
-import akka.pattern.Patterns;
+
 import java.util.concurrent.Future;
 
 public class TesterRoutes extends AllDirectives {
