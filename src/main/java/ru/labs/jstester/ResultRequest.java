81b285b2f842a4adbf4a12727032a0f246237b08
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-01 23:28:00 +0300

    Scripted auto-commit on change (2019-11-01 23:28:00) by gitwatch.sh

--- src/main/java/ru/labs/jstester/ResultRequest.java
+++ src/main/java/ru/labs/jstester/ResultRequest.java
@@ -1,4 +1,13 @@
 package ru.labs.jstester;
 
 public class ResultRequest {
+    private String packageID;
+
+    public ResultRequest(String packageID) {
+        this.packageID = packageID;
+    }
+
+    public String getPackageID() {
+        return packageID;
+    }
 }
