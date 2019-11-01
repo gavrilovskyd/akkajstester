689390a9dfd6aef89a6ac9893b73a0d01fa9ccf9
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-01 22:35:41 +0300

    Scripted auto-commit on change (2019-11-01 22:35:41) by gitwatch.sh

--- src/main/java/ru/labs/jstester/JSTesterApp.java
+++ src/main/java/ru/labs/jstester/JSTesterApp.java
@@ -6,10 +6,13 @@ import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
+
+import java.util.concurrent.CompletionStage;
 
 public class JSTesterApp {
     public static void main(String[] args) throws Exception {
@@ -19,10 +22,10 @@ public class JSTesterApp {
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         HTTPServer server = new HTTPServer();
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = server.createRoute().flow(system, materializer);
-        http.bindAndHandle(routeFlow, ConnectHttp.toHost("localhost", 8080), materializer);
+        final CompletionStage<ServerBinding> binding =  http.bindAndHandle(routeFlow, ConnectHttp.toHost("localhost", 8080), materializer);
 
         System.out.println("Server started at http://localhost:8080/");
         System.in.read();
-        
+
     }
 }
