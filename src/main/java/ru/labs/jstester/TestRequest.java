e6442165a101e1eb1a2af06b88290a46b8cfdc30
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-02 02:35:04 +0300

    Scripted auto-commit on change (2019-11-02 02:35:04) by gitwatch.sh

--- src/main/java/ru/labs/jstester/TestRequest.java
+++ src/main/java/ru/labs/jstester/TestRequest.java
@@ -4,12 +4,17 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
-public class TestRequest extends TestMetaInfo {
+public class TestRequest {
+    private TestMetaInfo meta;
     private Test[] tests;
 
     public TestRequest(String packageID, String jsCode, String functionName, Test[] tests) {
-        super(packageID, jsCode, functionName);
+        this.meta = new TestMetaInfo(packageID, jsCode, functionName);
         this.tests = tests;
+    }
+
+    public TestMetaInfo getMeta() {
+        return meta;
     }
 
     public Test[] getTests() {
