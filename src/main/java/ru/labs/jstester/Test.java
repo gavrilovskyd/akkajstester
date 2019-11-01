6e10c66bfcf1ec98c6298c60624015025f67b95c
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-01 23:17:27 +0300

    Scripted auto-commit on change (2019-11-01 23:17:27) by gitwatch.sh

--- src/main/java/ru/labs/jstester/Test.java
+++ src/main/java/ru/labs/jstester/Test.java
@@ -4,4 +4,10 @@ public class Test {
     private String testName;
     private String expectedResult;
     private Object[] params;
+
+    public Test(String name, String expected, Object[] params) {
+        this.testName = name;
+        this.expectedResult = expected;
+        this.params = params;
+    }
 }
