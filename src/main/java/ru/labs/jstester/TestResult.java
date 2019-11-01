795a403f7074f00533a534f7957b5a5a479242bb
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-01 23:25:42 +0300

    Scripted auto-commit on change (2019-11-01 23:25:42) by gitwatch.sh

--- src/main/java/ru/labs/jstester/TestResult.java
+++ src/main/java/ru/labs/jstester/TestResult.java
@@ -2,6 +2,7 @@ package ru.labs.jstester;
 
 public class TestResult {
     private static final String OK_STATUS = "OK";
+    private static final String WRONG_ANSWER_STATUS = "WA";
 
     private Test test;
     private String actualResult;
