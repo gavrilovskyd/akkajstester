9158e5c88180bd8ed5d0ced803daad8522617e29
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-02 01:46:14 +0300

    Scripted auto-commit on change (2019-11-02 01:46:14) by gitwatch.sh

--- src/main/java/ru/labs/jstester/TesterRoutes.java
+++ src/main/java/ru/labs/jstester/TesterRoutes.java
@@ -38,7 +38,7 @@ public class TesterRoutes extends AllDirectives {
     private Route getSubmit(String packageID) {
         return get(()-> {
               Future<Object> possibleResult = Patterns.ask(requestRouter, new ResultRequest(packageID), TIMEOUT_MS);
-              return 
+              return
 
     });
     }
