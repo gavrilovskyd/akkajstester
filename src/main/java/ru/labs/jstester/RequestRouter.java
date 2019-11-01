6a0d9304ed4902d6c1fa60b90f8b2d0e9586ee25
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-02 00:11:23 +0300

    Scripted auto-commit on change (2019-11-02 00:11:23) by gitwatch.sh

--- src/main/java/ru/labs/jstester/RequestRouter.java
+++ src/main/java/ru/labs/jstester/RequestRouter.java
@@ -5,6 +5,7 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.japi.pf.ReceiveBuilder;
+import akka.routing.BalancingPool;
 
 public class RequestRouter extends AbstractActor {
     private ActorRef resultStorage;
@@ -13,7 +14,7 @@ public class RequestRouter extends AbstractActor {
         this.resultStorage = system.actorOf(Props.create(ResultsStorage.class), "result-storage");
 
         system.actorOf(
-                
+                new BalancingPool()
         )
     }
 
