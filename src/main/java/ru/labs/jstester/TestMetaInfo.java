50a48dcc2f4bc0c2ee569a700a17f838c48b87f9
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-02 00:48:29 +0300

    Scripted auto-commit on change (2019-11-02 00:48:29) by gitwatch.sh

--- src/main/java/ru/labs/jstester/TestMetaInfo.java
+++ src/main/java/ru/labs/jstester/TestMetaInfo.java
@@ -1,4 +1,11 @@
 package ru.labs.jstester;
 
 public class TestMetaInfo {
+    private String packageID;
+    private String jsCode;
+    private String functionName;
+
+    public TestMetaInfo(String packageID, String jsCode, String functionName) {
+        
+    }
 }
