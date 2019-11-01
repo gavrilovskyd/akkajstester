eb8c9a994234a40a3d374f375e33365b64fb032c
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-02 00:02:25 +0300

    Scripted auto-commit on change (2019-11-02 00:02:25) by gitwatch.sh

--- src/main/java/ru/labs/jstester/TestRunner.java
+++ src/main/java/ru/labs/jstester/TestRunner.java
@@ -29,7 +29,7 @@ public class TestRunner extends AbstractActor {
                             task.getFunctionName(), task.getTest().getParams()
                     ).toString(); // TODO: add timeout
 
-                    String status = ( output.equals(task.getTest().getExpectedResult()) ?  )
+                    String status = ( output.equals(task.getTest().getExpectedResult()) ? TestResult.OK_STATUS : TestResult.WRONG_ANSWER_STATUS );
 
                     this.resultStorage.tell(new TestResult(task.getTest(), output));
                 })
