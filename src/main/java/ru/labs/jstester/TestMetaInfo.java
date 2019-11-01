40d28e26413e5291c745e680994b5aef029bb406
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-02 02:31:21 +0300

    Scripted auto-commit on change (2019-11-02 02:31:21) by gitwatch.sh

--- src/main/java/ru/labs/jstester/TestMetaInfo.java
+++ src/main/java/ru/labs/jstester/TestMetaInfo.java
@@ -1,13 +1,12 @@
 package ru.labs.jstester;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 
+@JsonProperty(PropertyNamingStrategy.SNAKE_CASE.class)
 public class TestMetaInfo {
-    @JsonProperty("package_id")
     private String packageID;
-    @JsonProperty("js_code")
     private String jsCode;
-    @JsonProperty("function_name")
     private String functionName;
 
     public TestMetaInfo(String packageID, String jsCode, String functionName) {
