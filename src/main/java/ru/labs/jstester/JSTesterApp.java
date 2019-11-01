8d1032d1d662243f1546148bde587de1c100f18d
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-01 22:01:07 +0300

    Scripted auto-commit on change (2019-11-01 22:01:07) by gitwatch.sh

--- src/main/java/ru/labs/jstester/JSTesterApp.java
+++ src/main/java/ru/labs/jstester/JSTesterApp.java
@@ -3,11 +3,14 @@ package ru.labs.jstester;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
 
 public class JSTesterApp {
     public static void main(String[] args) throws Exception {
         ActorSystem system = ActorSystem.create("testing-system");
 
-
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
     }
 }
