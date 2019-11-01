d88aaeadfd6bc4cae2636eddb01a81a357444c77
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-02 02:55:39 +0300

    Scripted auto-commit on change (2019-11-02 02:55:39) by gitwatch.sh

--- src/main/java/ru/labs/jstester/TestRunner.java
+++ src/main/java/ru/labs/jstester/TestRunner.java
@@ -21,7 +21,7 @@ public class TestRunner extends AbstractActor {
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(TestTask.class, task -> {
-                    System.out.println(task.getMeta().toString());
+                    System.out.println(task.getMeta().getJsCode());
 
                     ScriptEngine engine = new ScriptEngineManager().getEngineByName(JS_ENGINE);
                     engine.eval(task.getMeta().getJsCode());
