e981850f3ddf70094d11fc0312a9cec3d4a91c31
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-01 22:32:11 +0300

    Scripted auto-commit on change (2019-11-01 22:32:11) by gitwatch.sh

--- src/main/java/ru/labs/jstester/JSTesterApp.java
+++ src/main/java/ru/labs/jstester/JSTesterApp.java
@@ -1,10 +1,14 @@
 package ru.labs.jstester;
 
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
 public class JSTesterApp {
     public static void main(String[] args) throws Exception {
@@ -12,7 +16,7 @@ public class JSTesterApp {
 
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-
         HTTPServer server = new HTTPServer();
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = server.createRoute().flow(system, materializer);
     }
 }
