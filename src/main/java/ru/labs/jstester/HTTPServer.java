fd303996d96df059d408a09bb88a7cb7edda15ca
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-02 00:56:30 +0300

    Scripted auto-commit on change (2019-11-02 00:56:29) by gitwatch.sh

--- src/main/java/ru/labs/jstester/HTTPServer.java
+++ src/main/java/ru/labs/jstester/HTTPServer.java
@@ -2,12 +2,15 @@ package ru.labs.jstester;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 import akka.http.javadsl.server.Route;
 
 public class HTTPServer {
     private TesterRoutes testerRoutes;
 
     public HTTPServer(ActorSystem system) {
+        system.actorOf(Props.create(RequestRouter.class), "request-router");
+
         testerRoutes = new TesterRoutes(requestRouter);
     }
 
