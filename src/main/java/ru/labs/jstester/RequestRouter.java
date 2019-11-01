49ea8a7c5cb77a6c424ad932ceae523c4cdedc5a
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-02 00:08:06 +0300

    Scripted auto-commit on change (2019-11-02 00:08:06) by gitwatch.sh

--- src/main/java/ru/labs/jstester/RequestRouter.java
+++ src/main/java/ru/labs/jstester/RequestRouter.java
@@ -4,6 +4,8 @@ import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
 public class RequestRouter extends AbstractActor {
+    
+
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
