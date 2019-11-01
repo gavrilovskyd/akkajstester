0e8a617314f3aa2c7b60cababc58444af9d81249
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-01 09:16:27 +0300

    Scripted auto-commit on change (2019-11-01 09:16:27) by gitwatch.sh

--- src/main/java/ru/labs/jstester/StorageActor.java
+++ src/main/java/ru/labs/jstester/StorageActor.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 public class StorageActor extends AbstractActor {
     private HashMap<String, String> storage = new HashMap<>();
 
+    
 
     @Override
     public Receive createReceive() {
