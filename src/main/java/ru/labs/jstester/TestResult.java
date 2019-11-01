215f24f0110fc534c6b3693d7e4fa6b9aab421a8
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-01 23:23:52 +0300

    Scripted auto-commit on change (2019-11-01 23:23:52) by gitwatch.sh

--- src/main/java/ru/labs/jstester/TestResult.java
+++ src/main/java/ru/labs/jstester/TestResult.java
@@ -1,7 +1,13 @@
 package ru.labs.jstester;
 
 public class TestResult {
+    public enum VERDICT {
+        OK,
+        WA,
+        TL,
+    };
+
     private Test test;
     private String actualResult;
-    private 
+    private
 }
