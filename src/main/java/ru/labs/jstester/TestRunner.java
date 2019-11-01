f6d6f32d36f1c65cda42538c6c0205c2aaafc94e
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-02 00:00:56 +0300

    Scripted auto-commit on change (2019-11-02 00:00:56) by gitwatch.sh

--- src/main/java/ru/labs/jstester/TestRunner.java
+++ src/main/java/ru/labs/jstester/TestRunner.java
@@ -29,7 +29,7 @@ public class TestRunner extends AbstractActor {
                             task.getFunctionName(), task.getTest().getParams()
                     ).toString(); // TODO: add timeout
 
-
+                    this.resultStorage.tell(new TestResult(task.getTest(), ));
                 })
                 .build();
     }
