4cc852f0c093c763ffa89234915795d334822f65
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-01 23:52:56 +0300

    Scripted auto-commit on change (2019-11-01 23:52:56) by gitwatch.sh

--- src/main/java/ru/labs/jstester/TestRunner.java
+++ src/main/java/ru/labs/jstester/TestRunner.java
@@ -5,8 +5,6 @@ import akka.japi.pf.ReceiveBuilder;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
-import java.util.ArrayList;
-import java.util.List;
 
 public class TestRunner extends AbstractActor {
     private final static String JS_ENGINE = "nashorn";
@@ -16,6 +14,7 @@ public class TestRunner extends AbstractActor {
         return ReceiveBuilder.create()
                 .match(Test.class, r -> {
                     ScriptEngine engine = new ScriptEngineManager().getEngineByName(JS_ENGINE);
+                    engine.eval()
                 })
                 .build();
     }
