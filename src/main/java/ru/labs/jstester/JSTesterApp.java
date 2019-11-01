06c0dd5667bc3c4a536acd8cdc22c9b997128306
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-01 08:59:06 +0300

    Scripted auto-commit on change (2019-11-01 08:59:06) by gitwatch.sh

--- src/main/java/ru/labs/jstester/JSTesterApp.java
+++ src/main/java/ru/labs/jstester/JSTesterApp.java
@@ -1,7 +1,9 @@
 package ru.labs.jstester;
 
+import akka.actor.ActorSystem;
+
 public class JSTesterApp {
     public static void main(String[] args) throws Exception {
-
+        ActorSystem = 
     }
 }
