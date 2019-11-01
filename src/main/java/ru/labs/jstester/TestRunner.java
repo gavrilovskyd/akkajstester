d208f05764e54732249f2b4d335a622c5832b4ad
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-01 23:47:45 +0300

    Scripted auto-commit on change (2019-11-01 23:47:45) by gitwatch.sh

--- src/main/java/ru/labs/jstester/TestRunner.java
+++ src/main/java/ru/labs/jstester/TestRunner.java
@@ -1,6 +1,35 @@
 package ru.labs.jstester;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestRunner extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(TestResult.class, r -> {
+                    List<TestResult> results = innerStorage.get(r.getTest().getPackageID());
+
+                    if (results == null) {
+                        results = new ArrayList<>();
+                        results.add(r);
+                        innerStorage.put(r.getTest().getPackageID(), results);
+                    } else {
+                        results.add(r);
+                    }
+                })
+                .match(ResultRequest.class, r -> {
+                    List<TestResult> results = innerStorage.get(r.getPackageID());
+
+                    if (results == null) {
+                        getSender().tell(new TestResult[]{}, getSelf());
+                    } else {
+                        getSender().tell(results.toArray(), getSelf()); // TODO: add sort by innerID
+                    }
+                })
+                .build();
+    }
 }
