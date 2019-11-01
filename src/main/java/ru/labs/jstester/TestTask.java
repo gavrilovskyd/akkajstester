997e62033b8803e272eb2019b1e3bcfb9c5b5708
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-01 23:19:54 +0300

    Scripted auto-commit on change (2019-11-01 23:19:54) by gitwatch.sh

--- src/main/java/ru/labs/jstester/TestTask.java
+++ src/main/java/ru/labs/jstester/TestTask.java
@@ -1,8 +1,8 @@
 package ru.labs.jstester;
 
 public class TestTask {
-    private int packageID;
+    private String packageID;
     private String jsCode;
     private String functionName;
-    
+
 }
