9d591d01fb5c46811ac9a73c4064987880cd4b7d
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-02 00:05:51 +0300

    Scripted auto-commit on change (2019-11-02 00:05:51) by gitwatch.sh

--- src/main/java/ru/labs/jstester/TestRequest.java
+++ src/main/java/ru/labs/jstester/TestRequest.java
@@ -4,13 +4,13 @@ public class TestRequest {
     private String packageID;
     private String jsCode;
     private String functionName;
-    private Test[] test;
+    private Test[] tests;
 
-    public TestRequest(String packageID, String jsCode, String functionName, Test test) {
+    public TestRequest(String packageID, String jsCode, String functionName, Test[] tests) {
         this.packageID = packageID;
         this.jsCode = jsCode;
         this.functionName = functionName;
-        this.test = test;
+        this.tests = tests;
     }
 
     public String getPackageID() {
