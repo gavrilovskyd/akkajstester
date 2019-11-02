23ce80480fde17574109208906b627c4e5c5a9d6
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-02 10:49:22 +0300

    Scripted auto-commit on change (2019-11-02 10:49:22) by gitwatch.sh

--- src/main/java/ru/labs/jstester/messages/TextResponseMessage.java
+++ src/main/java/ru/labs/jstester/messages/TextResponseMessage.java
@@ -1,5 +1,17 @@
 package ru.labs.jstester.messages;
 
-public class BasicResponseMessage {
-    private String 
+public class TextResponseMessage {
+    private String message;
+
+    public TextResponseMessage() {
+        this.message = "";
+    }
+
+    public TextResponseMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
