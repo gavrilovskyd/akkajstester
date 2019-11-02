9ad27e37d38a9428b2cdceb3311358c78e22ca6e
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-02 10:49:48 +0300

    Scripted auto-commit on change (2019-11-02 10:49:48) by gitwatch.sh

--- src/main/java/ru/labs/jstester/messages/TextResponseMessage.java
+++ src/main/java/ru/labs/jstester/messages/TextResponseMessage.java
@@ -1,5 +1,9 @@
 package ru.labs.jstester.messages;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class TextResponseMessage {
     private String message;
 
