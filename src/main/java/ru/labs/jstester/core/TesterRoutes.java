89e32f65451957cdd71f3ffca952c57b7903a361
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-02 10:51:33 +0300

    Scripted auto-commit on change (2019-11-02 10:51:33) by gitwatch.sh

--- src/main/java/ru/labs/jstester/core/TesterRoutes.java
+++ src/main/java/ru/labs/jstester/core/TesterRoutes.java
@@ -67,10 +67,9 @@ public class TesterRoutes extends AllDirectives {
                                   }
 
 
-                                      byte[] marshaled = mapper.writeValueAsBytes(parameter);
-
-                                      return HttpResponse.create().withStatus(StatusCodes.OK)
-                                              .withEntity(HttpEntities.create(ContentTypes.APPLICATION_JSON, marshaled));
+                                  byte[] marshaled = mapper.writeValueAsBytes(parameter);
+                                  return HttpResponse.create().withStatus(StatusCodes.OK)
+                                          .withEntity(HttpEntities.create(ContentTypes.APPLICATION_JSON, marshaled));
                               } catch (JsonProcessingException e) {
                                   logger.error("got json processing exception {}", e.getMessage());
                                   return HttpResponse.create().withStatus(StatusCodes.INTERNAL_SERVER_ERROR)
