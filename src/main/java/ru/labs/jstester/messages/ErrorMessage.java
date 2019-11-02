f67bc2e5b90942c84e21b0ea34160b3025b771b9
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-02 03:45:06 +0300

    Scripted auto-commit on change (2019-11-02 03:45:06) by gitwatch.sh

--- src/main/java/ru/labs/jstester/messages/ErrorMessage.java
+++ src/main/java/ru/labs/jstester/messages/ErrorMessage.java
@@ -1,4 +1,8 @@
 package ru.labs.jstester.messages;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class ErrorMessage {
 }
