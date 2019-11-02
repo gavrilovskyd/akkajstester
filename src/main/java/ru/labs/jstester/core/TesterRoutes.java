72c924bb80b54db618e90c3ac471b9e518774d6a
Daniil Gavrilovsky <d.gavrilovsky@corp.mail.ru>
2019-11-02 03:47:12 +0300

    Scripted auto-commit on change (2019-11-02 03:47:12) by gitwatch.sh

--- src/main/java/ru/labs/jstester/core/TesterRoutes.java
+++ src/main/java/ru/labs/jstester/core/TesterRoutes.java
@@ -49,7 +49,6 @@ public class TesterRoutes extends AllDirectives {
                       .map(new Mapper<Object, HttpResponse>(){
                           @Override
                           public HttpResponse apply(Object parameter) {
-                              ObjectMapper mapper = new ObjectMapper();
                               if (!(parameter instanceof TestResult[])) {
                                   logger.error("wrong future parameter {}, expected TestResult[]",
                                           parameter.getClass().toString());
@@ -57,6 +56,7 @@ public class TesterRoutes extends AllDirectives {
                                           .withEntity("Wooops!!!");
                               }
 
+                              ObjectMapper mapper = new ObjectMapper();
                               TestResult[] results = ((TestResult[])parameter);
                               if (results.length == 0) {
                                   logger.warning("submit {} does not found", packageID);
